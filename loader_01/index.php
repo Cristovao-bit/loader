7cb7904bcbb3eb5bbcbd54eb40363a13f68a0cd6
Cristovao-bit <63509410+Cristovao-bit@users.noreply.github.com>
2020-06-15 00:20:57 -0300

    origin

---
+++ loader_01/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Loader Animate</title>
+        <link rel="stylesheet" href="_css/style.css"/>
+    </head>
+    <body>
+        <div class="center">
+            <div class="text">LOADING</div>
+            <div class="ring"></div>
+        </div>
+    </body>
+</html>
---
+++ loader_02/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Gráficos de progresso cricular</title>
+        <link rel="stylesheet" href="_css/style.css"/>
+    </head>
+    <body>
+        <h1>Barra de Progresso</h1>
+        
+        <div class="circular-progress">
+            
+        </div>
+    </body>
+</html>
